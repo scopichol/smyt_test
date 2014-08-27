8b70dcb30385257ba2583dd8afd513ed89c9e0ff
Oleg Scopich <scopichol@gmail.com>
2014-08-27 14:04:55 +0300

    change dbname

--- smyt/settings.py
+++ smyt/settings.py
@@ -58,7 +58,7 @@ WSGI_APPLICATION = 'smyt.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db_smyt.sqlite3'),
     }
 }
 
